089e0473fda3e8d86e40b1f7735d6e1bf9d4bf74
Graham Binns <graham@outcoded.uk>
2015-12-30 18:27:14 +0000

    PEP8-ified bb8.py.

--- src/bb8.py
+++ src/bb8.py
@@ -1,68 +1,69 @@
-""" driver by Alistair Buxton <a.j.buxton@gmail.com>"""
+""" driver by alistair buxton <a.j.buxton@gmail.com>"""
 
 import btle
 
 
-class BB8(btle.DefaultDelegate):
+class bB8(btle.default_delegate):
 
-    def __init__(self, deviceAddress):
-        btle.DefaultDelegate.__init__(self)
+    def __init__(self, device_address):
+        btle.default_delegate.__init__(self)
 
-        # Address type must be "random" or it won't connect.
-        self.peripheral = btle.Peripheral(deviceAddress, btle.ADDR_TYPE_RANDOM)
-        self.peripheral.setDelegate(self)
+        # address type must be "random" or it won't connect.
+        self.peripheral = btle.peripheral(
+            device_address, btle.ADDR_TYPE_RANDOM)
+        self.peripheral.set_delegate(self)
 
         self.seq = 0
 
-        # Attribute UUIDs are identical to Ollie.
-        self.antidos = self.getSpheroCharacteristic('2bbd')
-        self.wakecpu = self.getSpheroCharacteristic('2bbf')
-        self.txpower = self.getSpheroCharacteristic('2bb2')
-        self.roll = self.getSpheroCharacteristic('2ba1')
-        self.notify = self.getSpheroCharacteristic('2ba6')
+        # attribute uUIDs are identical to ollie.
+        self.antidos = self.get_sphero_characteristic('2bbd')
+        self.wakecpu = self.get_sphero_characteristic('2bbf')
+        self.txpower = self.get_sphero_characteristic('2bb2')
+        self.roll = self.get_sphero_characteristic('2ba1')
+        self.notify = self.get_sphero_characteristic('2ba6')
 
-        # This startup sequence is also identical to the one for Ollie.
-        # It even uses the same unlock code.
-        print 'Sending antidos'
-        self.antidos.write('011i3', withResponse=True)
-        print 'Sending txpower'
-        self.txpower.write('\x0007', withResponse=True)
-        print 'Sending wakecpu'
-        self.wakecpu.write('\x01', withResponse=True)
+        # this startup sequence is also identical to the one for ollie.
+        # it even uses the same unlock code.
+        print 'sending antidos'
+        self.antidos.write('011i3', with_response=True)
+        print 'sending txpower'
+        self.txpower.write('\x0007', with_response=True)
+        print 'sending wakecpu'
+        self.wakecpu.write('\x01', with_response=True)
 
-    def getSpheroCharacteristic(self, fragment):
-        return self.peripheral.getCharacteristics(
+    def get_sphero_characteristic(self, fragment):
+        return self.peripheral.get_characteristics(
             uuid='22bb746f' + fragment + '75542d6f726568705327')[0]
 
-    def dumpCharacteristics(self):
-        for s in self.peripheral.getServices():
+    def dump_characteristics(self):
+        for s in self.peripheral.get_services():
             print s
-            for c in s.getCharacteristics():
+            for c in s.get_characteristics():
                 print c, hex(c.handle)
 
-    def cmd(self, did, cid, data=[], answer=True, resetTimeout=True):
-        # Commands are as specified in Sphero API 1.50 PDF.
-        # https://github.com/orbotix/DeveloperResources/
+    def cmd(self, did, cid, data=[], answer=True, reset_timeout=True):
+        # commands are as specified in sphero aPI 1.50 pDF.
+        # https://github.com/orbotix/developer_resources/
         seq = (self.seq & 255)
         self.seq += 1
         sop2 = 0xfc
         sop2 |= 1 if answer else 0
-        sop2 |= 2 if resetTimeout else 0
+        sop2 |= 2 if reset_timeout else 0
         dlen = len(data) + 1
         chk = (sum(data) + did + cid + seq + dlen) & 255
         chk ^= 255
 
         msg = [0xff, sop2, did, cid, seq, dlen] + data + [chk]
         print 'cmd:', ' '.join([chr(c).encode('hex') for c in msg])
-        # Note: withResponse is very important. Most commands won't work
+        # note: with_response is very important. most commands won't work
         # without it.
-        self.roll.write(''.join([chr(c) for c in msg]), withResponse=True)
+        self.roll.write(''.join([chr(c) for c in msg]), with_response=True)
 
-    def handleNotification(self, cHandle, data):
-        print 'Notification:', cHandle, data.encode('hex')
+    def handle_notification(self, c_handle, data):
+        print 'notification:', c_handle, data.encode('hex')
 
-    def waitForNotifications(self, time):
-        self.peripheral.waitForNotifications(time)
+    def wait_for_notifications(self, time):
+        self.peripheral.wait_for_notifications(time)
 
     def disconnect(self):
         self.peripheral.disconnect()
@@ -70,20 +71,20 @@ class BB8(btle.DefaultDelegate):
 
 if __name__ == '__main__':
 
-    # Connect by address. Use "sudo hcitool lescan" to find address.
-    bb = BB8('cd:9b:6c:96:6b:10')
+    # connect by address. use "sudo hcitool lescan" to find address.
+    bb = bB8('cd:9b:6c:96:6b:10')
 
-    # Dump all GATT stuff.
-    # bb.dumpCharacteristics()
+    # dump all gATT stuff.
+    # bb.dump_characteristics()
 
-    # Request some sensor stream.
+    # request some sensor stream.
     bb.cmd(0x02, 0x11, [0, 80, 0, 1, 0x80, 0, 0, 0, 0])
 
     for i in range(255):
-        # Set RGB LED colour.
+        # set rGB lED colour.
         bb.cmd(0x02, 0x20, [254, i, 2, 0])
-        # Wait for streamed data.
-        bb.waitForNotifications(1.0)
+        # wait for streamed data.
+        bb.wait_for_notifications(1.0)
 
-    # Must manually disconnect or you won't be able to reconnect.
+    # must manually disconnect or you won't be able to reconnect.
     bb.disconnect()
